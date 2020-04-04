460a9e059d202c34de7bb5d2cc21096fde09cd02
Den <denisanatoliyovich@gmail.com>
2020-04-04 11:42:27 +0300

    Add parallel computing

--- Lab2.2.py
+++ Lab2.2.py
@@ -9,6 +9,8 @@ import numpy as np
 import random
 from math import sin, cos, pi
 import matplotlib.pyplot as plt
+import timeit
+import concurrent.futures
 
 def generateWTable(N):
 
@@ -37,7 +39,6 @@ def calculateFFT(N, x):
     
     F = np.zeros(N)
     
-    # calculate odd and even separately
     for p in range(N // 2):
         for k in range(N // 2):
             FEven[p] += x[2 * k] * wTable[p][k]
@@ -52,6 +53,70 @@ def calculateFFT(N, x):
     
     return F
 
+    
+def calcFOdd(N, wTable, x):
+    FOdd = np.zeros(N // 2)
+    
+    for p in range(N // 2):
+            for k in range(N // 2):
+                FOdd[p] += x[2 * k + 1] * wTable[p][k]
+    return FOdd
+
+
+def calcFEven(N, wTable, x):
+    FEven = np.zeros(N // 2)
+    
+    for p in range(N // 2):
+            for k in range(N // 2):
+                FEven[p] += x[2 * k] * wTable[p][k]
+    return FEven
+
+
+def calcFirstHalf(N, FEven, FOdd, newTable):
+    n = N//2
+    F = np.zeros(n)
+    
+    for p in range(n):
+        F[p] += FEven[p] + newTable[p] * FOdd[p]
+    
+    return F
+    
+
+def calcSecondHalf(N, FEven, FOdd, newTable):
+    n = N//2
+    F = np.zeros(n)
+    for p in range(n , N):
+        F[p-n] += FEven[p - n] - newTable[p] * FOdd[p - n]
+    
+    return F
+
+
+def calculateFFTParallel(N, x):
+    
+    wTable = generateWTable(N)
+    newTable = generateNewWTable(N)
+    
+    FOdd = np.zeros(N // 2)
+    
+    FEven = np.zeros(N // 2)
+    
+    F = np.zeros(N)
+    
+    # calculate odd and even separately
+    with concurrent.futures.ThreadPoolExecutor(2) as executor:
+        args = [N, wTable, x]
+        
+        calcFOddRes = executor.submit( lambda p: calcFOdd(*p), args)
+        calcFEvenRes = executor.submit( lambda p: calcFEven(*p), args)
+        FOdd = calcFOddRes.result()
+        FEven = calcFEvenRes.result()
+        
+        args2 = [N, FEven, FOdd, newTable]
+        
+        calcFirstHalfRes = executor.submit( lambda p: calcFirstHalf(*p), args2)
+        calcSecondHalfRes = executor.submit( lambda p: calcSecondHalf(*p), args2)
+        F = calcFirstHalfRes.result() + calcSecondHalfRes.result()
+    
 
 def generateX(n, time, W):
     xValues = [0] * time
@@ -67,13 +132,25 @@ def generateX(n, time, W):
 
 def main():
     n = 8
-    N = 1024
+    N = 30000
     W = 1200
     
     xValues = generateX(n, N, W)
     
+    time_fft1 = timeit.default_timer()
     fftValues = calculateFFT(N, xValues)
-        
+    time_fft2 = timeit.default_timer()
+    
+    time_fftParallel1 = timeit.default_timer()
+    fftValuesParallel = calculateFFTParallel(N, xValues)
+    time_fftParallel2 = timeit.default_timer()
+    print("N:")
+    print(N)
+    print('usual')    
+    print(time_fft2-time_fft1)
+    print("Parallel")
+    print(time_fftParallel2-time_fftParallel1)
+    
     figure, (plotXValues, plotFFTValues) = plt.subplots(2, figsize=(20, 20))
     plotXValues.plot(range(N), xValues, "b")
     plotXValues.title.set_text("Згенерований сигнал X")
